b84d2e5f40d8265223f0287fdc930a7d059f955f
NiklasTR <niklastimon@gmail.com>
2019-03-12 11:18:09 -0400

    added template and clean sh grouper

---
+++ R/generate_group.R
@@ -0,0 +1,27 @@
+#' Generate grouping function call
+#'
+#' @param plate_name
+#' @param channel_n
+#'
+#' @return
+#' @export
+#'
+#' @examples
+generate_group <- function(plate_name, channel_n){
+
+# expanding combinations
+df <- expand.grid(plate_name, channel_n) %>%
+  as_tibble() %>%
+  magrittr::set_colnames(c("plate_name", "channel_n"))
+
+# creating bash script
+c("#!/bin/sh",
+paste0("python /home/ubuntu/bucket/metadata/ManualMetadata_dir.py /home/ubuntu/bucket/metadata/",
+       df$plate_name,
+       "/ ",
+       read_lines("group_template.txt"),
+       " ",
+       df$channel_n)
+) %>%
+  write_lines(paste0(plate_name[1], "_create_group.sh"))
+}
