c86f48c5979b1396ba00aaf70aa0f3ce8feaf116
NiklasTR <niklastimon@gmail.com>
2019-03-18 15:47:11 -0400

    database writing

---
+++ R/dump_database.R
@@ -0,0 +1,21 @@
+#' Dump a dataframe into a database
+#'
+#' @param path_data
+#' @param channel_of_interest
+#' @param measurment_of_interest
+#'
+#' @return
+#' @export
+#' @import DBI
+#' @import janitor
+#' @import stringr
+#' @import pool
+#'
+#' @examples
+dump_database <- function(x, name_of_table = "cells", pool = pool){
+  x %>%
+    read_csv %>%
+    mutate(id = stringr::str_split(x, pattern = "/") %>% unlist() %>% .[8]) %>%
+  janitor::clean_names() %>%
+  dbWriteTable(pool, name_of_table, ., append = TRUE)
+}
