5b3db25a8edfeb56ef4881637d4bb4577c6ce847
NiklasTR <niklastimon@gmail.com>
2019-03-21 14:33:57 -0400

    added new replicate

--- R/collect_feature_data_db.R
+++ R/collect_feature_data_db.R
@@ -21,7 +21,7 @@ collect_feature_data_db <- function(path_data,
   pool <- pool::dbPool(RSQLite::SQLite(), dbname = database)
   files <- list.files(path_data, pattern = channel_of_interest, full.names = TRUE) %>% paste0(., "/", measurment_of_interest)
 
-  tmp <- files %>% lapply(dump_database)
+  tmp <- files %>% lapply(dump_database, con = pool)
 
   # giving the database back
   timestamp <- Sys.time() %>% stringr::str_remove_all(" ") %>% stringr::str_remove_all(":")
