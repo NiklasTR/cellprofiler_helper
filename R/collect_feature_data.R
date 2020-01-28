f90897edc1dcbf37bd27828ddb6e293ed7849dac
Niklas <cclf@ip-172-31-4-120.us-east-2.compute.internal>
2020-01-28 18:06:46 +0000

    removed metadata

--- R/collect_feature_data.R
+++ R/collect_feature_data.R
@@ -10,13 +10,17 @@
 #' @import janitor
 #'
 #' @examples
-collect_feature_data <- function(path_data, channel_of_interest = "ch1", measurment_of_interest = "measurment_IdentifySecondaryObjects.csv"){
+collect_feature_data <- function(path_data,
+                                 channel_of_interest = "ch1",
+                                 result_path = "~/dcp_helper/data/results/",
+                                 measurment_of_interest = "measurment_IdentifySecondaryObjects.csv"){
   #path_data <- "~/bucket/flatfield/000012070903__2019-01-10T20_04_27-Measurement_3/"
   #channel_of_interest = "ch1"
   #measurment_of_interest = "measurment_IdentifySecondaryObjects.csv"
 
   barcode <- path_data %>% str_extract(pattern = "0000\\d+__\\d+-\\d\\d-\\d+T\\d+_\\d+_\\d+-Measurement_\\d")
 
+  # run this command first: aws s3 sync s3://ascstore/flatfield/ ~/dcp_helper/data/results/ --exclude "*" --include "*.csv" --force-glacier-transfer
 
   barcode %>%
     paste0("aws s3 ls s3://ascstore/flatfield/", ., "/ > processed_wells.txt") %>%
@@ -25,7 +29,7 @@ collect_feature_data <- function(path_data, channel_of_interest = "ch1", measurm
   processed_wells <- read_table2("processed_wells.txt",
                                  col_names = FALSE) %>% janitor::clean_names() %>%
     filter(grepl(x2, pattern = channel_of_interest)) %>%
-    transmute(full_path = paste0("~/bucket/flatfield/", barcode, "/", x2, measurment_of_interest),
+    transmute(full_path = paste0(result_path, barcode, "/", x2, measurment_of_interest),
               barcode) %>%
     #head(10) %>%
     # reading all csv contents
