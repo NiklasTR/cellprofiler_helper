40d0e96b42f37a49954293b2f88e1dea49014026
Niklas <cclf@ip-172-31-4-120.us-east-2.compute.internal>
2020-01-30 16:26:42 +0000

    wiped metadata

--- metadata/000012095103__2019-09-04T17_26_03-Measurement_1/000012095103__2019-09-04T17_26_03-Measurement_1_create_group.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-pip install --user pandas
-python ~/dcp_helper/python/ManualMetadata_dir.py ~/bucket/metadata/000012095103__2019-09-04T17_26_03-Measurement_1/ "['Metadata_parent','Metadata_timepoint', 'Metadata_well', 'Metadata_fld', 'Metadata_channel']" pc
-python ~/dcp_helper/python/ManualMetadata_dir.py ~/bucket/metadata/000012095103__2019-09-04T17_26_03-Measurement_1/ "['Metadata_parent','Metadata_timepoint', 'Metadata_well', 'Metadata_fld', 'Metadata_channel']" bf
-python ~/dcp_helper/python/ManualMetadata_dir.py ~/bucket/metadata/000012095103__2019-09-04T17_26_03-Measurement_1/ "['Metadata_parent','Metadata_timepoint', 'Metadata_well', 'Metadata_fld', 'Metadata_channel']" ce
-python ~/dcp_helper/python/ManualMetadata_dir.py ~/bucket/metadata/000012095103__2019-09-04T17_26_03-Measurement_1/ "['Metadata_parent','Metadata_timepoint', 'Metadata_well', 'Metadata_fld', 'Metadata_channel']" tm
--- metadata/000012095103__2019-09-06T17_24_19-Measurement_3/000012095103__2019-09-06T17_24_19-Measurement_3_create_group.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-pip install --user pandas
-python ~/dcp_helper/python/ManualMetadata_dir.py ~/dcp_helper/metadata/000012095103__2019-09-06T17_24_19-Measurement_3/ "['Metadata_parent','Metadata_timepoint', 'Metadata_well', 'Metadata_fld', 'Metadata_channel']" pc
-python ~/dcp_helper/python/ManualMetadata_dir.py ~/dcp_helper/metadata/000012095103__2019-09-06T17_24_19-Measurement_3/ "['Metadata_parent','Metadata_timepoint', 'Metadata_well', 'Metadata_fld', 'Metadata_channel']" bf
-python ~/dcp_helper/python/ManualMetadata_dir.py ~/dcp_helper/metadata/000012095103__2019-09-06T17_24_19-Measurement_3/ "['Metadata_parent','Metadata_timepoint', 'Metadata_well', 'Metadata_fld', 'Metadata_channel']" ce
-python ~/dcp_helper/python/ManualMetadata_dir.py ~/dcp_helper/metadata/000012095103__2019-09-06T17_24_19-Measurement_3/ "['Metadata_parent','Metadata_timepoint', 'Metadata_well', 'Metadata_fld', 'Metadata_channel']" tm
